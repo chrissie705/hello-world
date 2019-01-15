cbbcfdcf7b9ecdaf73a1e21f8ac8178958e0ef6f
pingouin2k <tnguyen.hung@gmail.com>
2019-01-15 01:05:09 +0100

    BRANCH-SERVER-001 : recuperation par departements

--- node/app/fetcher.js
+++ node/app/fetcher.js
@@ -3,32 +3,41 @@ const axios = require('axios');
 const FormData = require('form-data');
 const querystring = require('querystring');
 const parser = require('./parser');
-const params = require('./urlParams.json');
+const params = require('./config/urlParams.json');
+const departements = require('./config/departements.json');
 
 function getRubrique(params) {
-  return async () => {
+  return async departement => {
     return await _.reduce(params.categories, async (acc, category) => {
       let collection = await acc;
-      const fetchRubriquePage = fetchRubrique.bind(null, params.url, category.params);
+      const fetchRubriquePage = fetchRubrique.bind(null, params.url, _.replace(category.params, '{departement}', departement));
       const result = await fetchRubriquePage();
 
       collection[category.label] = [result.data];
-      console.log(category.label, '=====================================================================');
-      console.log(result.data);
       const pageNb = await parser.getPageNumber(result.data);
-      console.log(category.label, '-----------', pageNb);
+      // get data by pages
       if (pageNb > 0) {
         collection = await _.reduce(_.range(2, pageNb), async (acc, page) => {
           const collection = await acc;
           const cat = await fetchRubriquePage(page);
-          console.log(category.label, '-----------', page);
           collection[category.label] = [...collection[category.label], cat.data];
           return acc;
         }, Promise.resolve(collection));
       }
       return collection;
     }, Promise.resolve({}));
-  }
+  };
+}
+
+function getRubriqueByDepartement(departement) {
+  const getRubriqueByParams = getRubrique(params);
+  return async () => {
+      return await _.reduce(departement, async (groupedDataDepartement, departement) => {
+      let collection = await groupedDataDepartement;
+      console.log(`=== DEPARTEMENT ${departement} ===`);
+      return _.merge(collection, await getRubriqueByParams(departement));
+    }, Promise.resolve({}));
+  };
 }
 
 async function fetchRubrique(url, values, nbpage = 1) {
@@ -38,4 +47,5 @@ async function fetchRubrique(url, values, nbpage = 1) {
 
 module.exports = {
   getRubrique: getRubrique(params),
+  getRubriqueByDepartement: getRubriqueByDepartement(departements),
 };
--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ const fetcher = require('./app/fetcher');
 const fs = require('async-file');
 
 async function getInfo(){
-  const data = await fetcher.getRubrique();
+  const data = await fetcher.getRubriqueByDepartement();
   await fs.writeFile(__dirname + '/test.json', JSON.stringify(data));
 }
 
